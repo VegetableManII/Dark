a22e19bbcd6337ac5209ea13f6d21e4729463081
VegetableManII <1343739480@qq.com>
2021-05-01 18:04:45 +0800

    v0

--- main.go
+++ main.go
@@ -8,16 +8,16 @@ import (
 func main() {
 	r := dark.New()
 	r.Get("/", func(context *dark.Context) {
-		context.HTML(http.StatusOK,"<h1>Dark Web Engine<h1>")
+		context.HTML(http.StatusOK, "<h1>Dark Web Engine<h1>\n")
 	})
 	r.Get("/hello", func(context *dark.Context) {
 		// 请求格式 /hello？name=xxx
-		context.String(http.StatusOK,"hello %s\nPath %s\n",context.Query("name"),context.Path)
+		context.String(http.StatusOK, "hello %s\nPath %s\n", context.Query("name"), context.Path)
 	})
 	r.POST("/login", func(c *dark.Context) {
-		c.JSON(http.StatusOK,dark.H{
+		c.JSON(http.StatusOK, dark.H{
 			"username": c.PostFrom("username"),
-			"password":c.PostFrom("password"),
+			"password": c.PostFrom("password"),
 		})
 	})
 	r.Run(":9999")
